06f41303f85a8eddebe9af1362dd538e4617e668
ArrayOne <1441576840@qq.com>
2017-01-04 23:36:46 +0800

    使用ng,加载数据（subnav）
    配置路由，页面加载

---
+++ js/gideFirst.js
@@ -0,0 +1,57 @@
+
+var app=angular.module('myApp',['ng','ngRoute']);
+
+app.config(function($routeProvider){
+  console.log(333);
+  $routeProvider
+    .when('/goodChoice',{
+      templateUrl:'tpl/goodChoice.html',
+    })
+
+    .when('/phone',{
+      templateUrl:'tpl/phone.html',
+    })
+
+
+    .when('/ear',{
+    templateUrl:"tpl/ear.html",
+    })
+
+    .when('/bell',{
+      templateUrl:'tpl/bell.html',
+    })
+
+    .when('/motion',{
+      templateUrl:'tpl/motion.html',
+    })
+
+    .when('/round',{
+      templateUrl:'tpl/round.html',
+
+    })
+
+    .when('/service',{
+      templateUrl:'tpl/service.html',
+    })
+
+    .when('/goodChoice',{
+      templateUrl:'tpl/goodChoice.html',
+    })
+
+    .when('/allItems',{
+      templateUrl:'tpl/allItems.html',
+    })
+
+
+  .otherwise({redirectTo:'/goodChoice'})
+
+});
+
+
+//设置一个父级控制器，便于页面的跳转
+
+app.controller('parentCtrl',['$scope','$location',function($scope,$location){
+  $scope.jump=function(arg){
+   $location.path(arg);
+  }
+}]);
